995d3476ee68b53d0b48ff0a4a682661d8667732
Gavriel Rachael-Homann <gavriel@gavriel.dev>
2022-04-16 21:38:49 -0500

    first commit

---
+++ init.lua
@@ -0,0 +1,60 @@
+require('plugins')
+
+vim.g.mapleader = ' '
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.swapfile = false
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.exrc = true
+vim.opt.hidden = true
+vim.opt.errorbells = false
+vim.opt.wrap = false
+vim.opt.smartcase = true
+vim.opt.backup = false
+vim.opt.undodir = '~/.vim/undodir'
+vim.opt.undofile = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.showmode = false
+vim.opt.completeopt = 'menuone,noinsert,noselect'
+vim.opt.colorcolumn = '80'
+vim.opt.signcolumn = 'yes'
+
+vim.api.nvim_set_keymap(
+    'n',
+    '<Leader>ff',
+    "<cmd>lua require('telescope.builtin').find_files()<CR>",
+    {noremap = true}
+)
+vim.api.nvim_set_keymap(
+    'n',
+    '<Leader>fg',
+    "<cmd>lua require('telescope.builtin').live_grep()<CR>",
+    {noremap = true}
+)
+vim.api.nvim_set_keymap(
+    'n',
+    '<Leader>fb',
+    "<cmd>lua require('telescope.builtin').buffers()<CR>",
+    {noremap = true}
+)
+vim.api.nvim_set_keymap(
+    'n',
+    '<Leader>fh',
+    "<cmd>lua require('telescope.builtin').help_tags()<CR>",
+    {noremap = true}
+)
+
+vim.cmd [[
+    colorscheme gruvbox
+    highlight Normal guibg=none
+]]
+
---
+++ lua/plugins.lua
@@ -0,0 +1,20 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use 'neovim/nvim-lspconfig'
+  use 'kabouzeid/nvim-lspinstall'
+
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use 'tpope/vim-fugitive'
+
+  use 'gruvbox-community/gruvbox'
+end)
+
